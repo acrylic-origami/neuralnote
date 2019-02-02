3a602af0a54c73f456c77e86114f83c40f5c4455
Derek Lam <dc.lam@mail.utoronto.ca>
2019-02-02 18:29:06 +0000

    Raw processing script

---
+++ reddit_pull/process_raw.py
@@ -0,0 +1,8 @@
+import psycopg2
+
+if __name__ == '__main__':
+	with psycopg2.connect('dbname=nl user=nl password=logbase') as conn:
+		cur = conn.cursor()
+		cur.execute('SELECT body FROM documents;')
+		for row in cur:
+			row[0]
